10f518cfbae7617052bba1fd8d7593ccb3816ecc
pjb57 <pjb5750@gmail.com>
2023-09-14 09:15:20 +0900

    layout 구성

---
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -0,0 +1,19 @@
+package com.godcoder.myhome.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+}
